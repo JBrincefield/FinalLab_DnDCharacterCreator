225bb01d1151dd449a287bfd7364822728022fe2
JBrincefield <jbrincefield@student.neumont.edu>
2024-02-28 10:59:03 -0700

    main created

---
+++ src/edu/neumont/csc150/Main.java
@@ -0,0 +1,13 @@
+package edu.neumont.csc150;
+
+/**
+ * @author jbrincefield
+ * @createdOn 2/28/2024 at 10:58 AM
+ * @projectName FinalLab_DnDCharacterCreator
+ * @packageName edu.neumont.csc150;
+ */
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
