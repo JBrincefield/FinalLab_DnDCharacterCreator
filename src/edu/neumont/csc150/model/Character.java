0c4340db2361ac320aa184d6a187b240f3a8acdf
JBrincefield <jbrincefield@student.neumont.edu>
2024-02-29 11:12:26 -0700

    updated character class/made die class
    added new vars to character class for stats and AC. removed setMaxHealth from constructor since stats will change health total. and made a die class that can roll any number of dice with any number of sides

--- src/edu/neumont/csc150/model/Character.java
+++ src/edu/neumont/csc150/model/Character.java
@@ -6,16 +6,26 @@ package edu.neumont.csc150.model;
  * @projectName FinalLab_DnDCharacterCreator
  * @packageName edu.neumont.csc150.model;
  */
-public class Character {
+public abstract class Character {
     private String name;
     private int maxHealth;
     private Race race;
+    int armourClass;
+    private int strength;
+    private int dexterity;
+    private int constitution;
+    private int intelligence;
+    private int wisdom;
+    private int charisma;
 
-    public void Character(String name, int maxHealth, Race race){
+
+    // I think that stats should be set in the individual classes, which means health will need to be as well. and AC
+    public void Character(String name, Race race){
         setName(name);
-        setMaxHealth(maxHealth);
         setRace(race);
     }
+
+    //region getters/setter
 
     public String getName() {
         return name;
@@ -40,6 +50,64 @@ public class Character {
     private void setRace(Race race) {
         this.race = race;
     }
+
+    private int getStrength() {
+        return strength;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+    public int getDexterity() {
+        return dexterity;
+    }
+
+    private void setDexterity(int dexterity) {
+        this.dexterity = dexterity;
+    }
+
+    public int getConstitution() {
+        return constitution;
+    }
+
+    private void setConstitution(int constitution) {
+        this.constitution = constitution;
+    }
+
+    public int getIntelligence() {
+        return intelligence;
+    }
+
+    private void setIntelligence(int intelligence) {
+        this.intelligence = intelligence;
+    }
+
+    public int getWisdom() {
+        return wisdom;
+    }
+
+    private void setWisdom(int wisdom) {
+        this.wisdom = wisdom;
+    }
+
+    public int getCharisma() {
+        return charisma;
+    }
+
+    private void setCharisma(int charisma) {
+        this.charisma = charisma;
+    }
+
+    public int getArmourClass() {
+        return armourClass;
+    }
+
+    private void setArmourClass(int armourClass) {
+        this.armourClass = armourClass;
+    }
+
+    //endregion
 
     @Override
     public String toString() {
---
+++ src/edu/neumont/csc150/model/Die.java
@@ -0,0 +1,38 @@
+package edu.neumont.csc150.model;
+
+import java.util.Random;
+
+/**
+ * @author jbrincefield
+ * @createdOn 2/29/2024 at 11:03 AM
+ * @projectName FinalLab_DnDCharacterCreator
+ * @packageName edu.neumont.csc150.model;
+ */
+public class Die {
+    Random random = new Random();
+    private static final int MIN_NUM_SIDES = 2, MIN_NUM_DICE = 1;
+
+    public int roll(int numSides){
+        if (numSides < MIN_NUM_SIDES)
+            throw new IllegalArgumentException("The number of sides on a die MUST be at least " + MIN_NUM_SIDES);
+
+        int returnRoll = random.nextInt(1, numSides + 1);
+
+        return returnRoll;
+    }
+
+    public int roll(int numDice, int numSides){
+        if (numDice < MIN_NUM_DICE)
+            throw new IllegalArgumentException("The number of dice MUST be at least " + MIN_NUM_DICE);
+        if (numSides < MIN_NUM_SIDES)
+            throw new IllegalArgumentException("The number of sides on a die MUST be at least " + MIN_NUM_SIDES);
+
+        int rollSum = 0;
+
+        for (int i = 0; i < numDice; i++) {
+            rollSum += random.nextInt(1, numSides + 1);
+        }
+
+        return rollSum;
+    }
+}
