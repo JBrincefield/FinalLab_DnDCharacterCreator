d726ba91fe3195e01081861f712de835ea4f1dff
JBrincefield <jbrincefield@student.neumont.edu>
2024-03-03 17:10:08 -0700

    character stuffs

--- src/edu/neumont/csc150/model/character/Wizard.java
+++ src/edu/neumont/csc150/model/character/Wizard.java
@@ -3,6 +3,7 @@ package edu.neumont.csc150.model.character;
 import edu.neumont.csc150.model.character.Character;
 import edu.neumont.csc150.model.item.Item;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -14,19 +15,22 @@ import java.util.List;
 public class Wizard extends Character {
 
 
+    public Wizard(){
+        this("test", Race.ELF, new ArrayList<Item>());
+    }
     public Wizard(String name, Race race, List<Item> backPack){
         super(name, race, backPack, 100);
-        rollStats();
-
-
-
-
+        setStats();
+        setMaxHealth(die.roll(6));
     }
 
-    private void rollStats(){
-        int[] tempStats = new int[6];
-
-
+    private void setStats(){
+        setIntelligence(die.roll(3,6));
+        setStrength(die.roll(3,6));
+        setCharisma(die.roll(3,6));
+        setWisdom(die.roll(3,6));
+        setConstitution(die.roll(3,6));
+        setDexterity(die.roll(3,6));
     }
 
 }
